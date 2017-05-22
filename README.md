d24ded484f481ce7a6a202b0fd96bfd4abf169e5
Samuel Huang <samhua@cisco.com>
2017-05-21 21:48:05 -0700

    Add extra artifacts

---
+++ README.md
@@ -0,0 +1,10 @@
+# oh-my-custom
+
+Collection of alias plugins written for [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh).
+
+Run `bash bootstrap.sh` to install these files to `~/.oh-my-zsh/custom`.
+
+Inspiration:
+
+- https://github.com/mathiasbynens/dotfiles
+- https://github.com/unixorn/awesome-zsh-plugins
---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git pull origin master
+
+function doIt() {
+    rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude ".osx" \
+        --exclude "bootstrap.sh" \
+        --exclude "README.md" \
+        --exclude "LICENSE.txt" \
+        -avh --no-perms . ~/.oh-my-zsh/custom;
+    source ~/.zshrc;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt;
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
+fi;
+unset doIt;
