e4a83742b4f6df432e2192a65842f8bffab833fb
Samuel Huang <samhuang91@gmail.com>
2017-07-07 19:56:02 -0700

    Refine bootstrap and rename plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-git pull origin master
-
 function doIt() {
-    rsync --exclude ".DS_Store" \
-        --exclude ".osx" \
-        --exclude "README.md" \
-        -avh --no-perms ./zsh-plugins/* ~/.oh-my-zsh/custom;
+    rsync --exclude '.DS_Store' \
+        --exclude '.osx' \
+        --exclude 'README.md' \
+        -avh --no-perms ./custom/* ~/.oh-my-zsh/custom
 }
 
-if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
-    doIt;
+if [ "$1" == '--force' ] || [ "$1" == '-f' ]; then
+    doIt
 else
-    read -r -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-    echo "";
+    read -r -p 'This may overwrite files. Are you sure? (y/n) ' -n 1
+    echo ''
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt;
-    fi;
-fi;
-unset doIt;
+        doIt
+    fi
+fi
+unset doIt
