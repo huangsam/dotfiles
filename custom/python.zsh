137d75454db0267b003d33ced3bbc95a4c5988f4
Samuel Huang <samhuang91@gmail.com>
2018-06-29 09:03:33 -0700

    Update python.zsh with venable

--- custom/python.zsh
+++ custom/python.zsh
@@ -1,5 +1,5 @@
 # Python helpers
-alias venv='source /usr/local/bin/virtualenvwrapper.sh'
+alias venable='source /usr/local/bin/virtualenvwrapper.sh'
 
 function pipup() {
     pip list --outdated | awk '{if (NR > 2) print $1"=="$3}' | xargs pip install
