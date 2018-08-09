13f22b7d85184e9aa5e41e713c4479467b597398
Samuel Huang <samhuang91@gmail.com>
2018-08-09 08:28:16 -0700

    Add alias for pipenv

--- custom/python.zsh
+++ custom/python.zsh
@@ -1,5 +1,6 @@
 # Python helpers
 alias venable='source /usr/local/bin/virtualenvwrapper.sh'
+alias penv='pipenv'
 
 function pipup() {
     pip list --outdated | awk '{if (NR > 2) print $1"=="$3}' | xargs pip install
