051b79e6f7f5ae769a9b63c5d98a7c459bf54899
Samuel Huang <samhuang91@gmail.com>
2019-04-06 10:24:27 -0700

    Add better comments to gerrit zsh

--- custom/gerrit.zsh
+++ custom/gerrit.zsh
@@ -1,8 +1,9 @@
-# Gerrit shortcuts
+# Publish changes to Gerrit SCM
 function gerrit_publish() {
     git push origin "HEAD:refs/for/$1"
 }
 
+# Create draft in Gerrit SCM
 function gerrit_draft() {
     git push origin "HEAD:refs/drafts/$1"
 }
