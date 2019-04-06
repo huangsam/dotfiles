f8d9db45ecbeb8069b8127fe43965289f9b9ad77
Samuel Huang <samhuang91@gmail.com>
2019-04-06 09:28:57 -0700

    Optimize and refine docker aliases

--- custom/docker.zsh
+++ custom/docker.zsh
@@ -3,11 +3,7 @@ alias dps='docker ps'
 alias dpsa='docker ps -a'
 alias dlog='docker log'
 alias dexec='docker exec'
-alias drm='docker rm'
-alias drma='docker rm $(docker ps -q -a)'
-alias drmav='docker rm -v $(docker ps -q -a)'
-alias drms='docker rm $(docker ps -q -f "status=exited")'
-alias drmsv='docker rm -v $(docker ps -q -f "status=exited")'
+alias drma='docker rm $(docker ps -qa)'
 alias dinip='docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"'
 alias dinim='docker inspect --format="{{range .NetworkSettings.Networks}}{{.MacAddress}}{{end}}"'
 alias dicfg='docker inspect --format="{{json .Config}}"'
@@ -15,11 +11,11 @@ alias dicfg='docker inspect --format="{{json .Config}}"'
 # Images
 alias di='docker images'
 alias drmi='docker rmi'
-alias drmia='docker rmi $(docker images -q -a)'
-alias drmid='docker rmi $(docker images -q -f "dangling=true")'
+alias drmia='docker rmi $(docker images -qa)'
+alias drmid='docker rmi $(docker images -qf "dangling=true")'
 
 function dpull() {
-    docker images | grep -v REPOSITORY | awk '{print $1 ":" $2}' | xargs -L1 docker pull
+    docker images --format '{{.Repository}}:{{.Tag }}' | xargs -L1 docker pull
 }
 
 # Volumes
@@ -29,7 +25,6 @@ alias drmvd='docker volume rm $(docker volume ls -q -f "dangling=true")'
 
 # Networks
 alias dnls='docker network ls'
-alias drmn='docker network rm'
 
 # Docker Compose
 alias fig='docker-compose'
