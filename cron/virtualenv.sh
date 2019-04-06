4c863477320848154fca0bcd17b8578fff790f2a
Samuel Huang <samhuang91@gmail.com>
2019-04-06 10:20:19 -0700

    Cleanup virtualenv with shellcheck

--- cron/virtualenv.sh
+++ cron/virtualenv.sh
@@ -5,11 +5,10 @@ VENV_PATH="${VENV_PATH:-$HOME/.local/share/virtualenvs}"
 
 # Run sync on each venv in workspace
 find "$VENV_PATH" -type l -exec rm {} +
-pushd "$VENV_PATH"
-venvs="$(ls)"
-for venv in $venvs ; do
-    echo $venv
+pushd "$VENV_PATH" || return
+for venv in * ; do
+    echo -n "Create symbolic links in $venv..."
     python -m venv "$venv"
-    echo "done."
+    echo 'done.'
 done
-popd
+popd || return
