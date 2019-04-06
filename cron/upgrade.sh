26e6a6fc091ef1f5814ca517ab1ca6062575a320
Samuel Huang <samhuang91@gmail.com>
2019-04-06 10:04:18 -0700

    Add comments to all cron jobs

--- cron/upgrade.sh
+++ cron/upgrade.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+# Establish commands
 BREW=/usr/local/bin/brew
 GIT=/usr/bin/git
+
+# Upgrade brew plugins
 $BREW upgrade && $BREW cleanup
+
+# Upgrade git plugins
 cd "$HOME/.oh-my-zsh" && $GIT pull
 cd "$HOME/.vim_runtime" && $GIT pull
+cd "$HOME/.dotfiles" && $GIT pull
--- cron/virtualenv.sh
+++ cron/virtualenv.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-VDIR="$HOME/.local/share/virtualenvs"
-find "$VDIR" -type l -exec rm {} +
-pushd "$VDIR"
+
+# Establish workspace of venv directories
+VENV_PATH="${VENV_PATH:-$HOME/.local/share/virtualenvs}"
+
+# Run sync on each venv in workspace
+find "$VENV_PATH" -type l -exec rm {} +
+pushd "$VENV_PATH"
 venvs="$(ls)"
 for venv in $venvs ; do
     echo $venv
