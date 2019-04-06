4f5a33c5f2ee5184124cad6fe52ac0953d1f448f
Samuel Huang <samhuang91@gmail.com>
2019-04-06 10:37:27 -0700

    Cleanup existing aliases with combine

--- combine.sh
+++ combine.sh
@@ -3,6 +3,9 @@
 # Establish bash aliases
 ALIAS_LOC="${1:-./bash_aliases}"
 
+# Wipe bash aliases clean
+test -f "$ALIAS_LOC" && rm -f "$ALIAS_LOC"
+
 # Combine alias files into one file
 for fl in custom/*.zsh
 do
