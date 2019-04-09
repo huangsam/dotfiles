a5446215dfad869aeb835f67d5172aa854692a7a
Samuel Huang <samhuang91@gmail.com>
2019-04-09 11:42:03 -0700

    Move python function to top of zsh

--- custom/python.zsh
+++ custom/python.zsh
@@ -1,11 +1,11 @@
-# Python helpers
-alias venable='source /usr/local/bin/virtualenvwrapper.sh'
-alias py='python'
-alias py3='python3'
-
 # Update available pip dependencies
 function pipup() {
     pip list --format=freeze --outdated \
         | awk -F'==' '{print $1}' \
         | xargs pip install -U
 }
+
+# Python helpers
+alias venable='source /usr/local/bin/virtualenvwrapper.sh'
+alias py='python'
+alias py3='python3'
