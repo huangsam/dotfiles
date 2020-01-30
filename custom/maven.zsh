1bd1b1d675732c5148c709643bfb66e99bac2296
Samuel Huang <samhuang91@gmail.com>
2020-01-30 08:57:19 -0800

    Remove some mvn aliases

--- custom/maven.zsh
+++ custom/maven.zsh
@@ -6,9 +6,3 @@ alias mdt='mvn dependency:tree'
 alias mdtml='mvn dependency:tree -DoutputType=graphml'
 alias mdtdot='mvn dependency:tree -DoutputType=dot'
 alias mdttgf='mvn dependency:tree -DoutputType=tgf'
-
-# Write Maven dependency listings to disk
-alias mdto='mvn dependency:tree -DoutputFile=mvn-deps.txt'
-alias mdtoml='mvn dependency:tree -DoutputType=graphml -DoutputFile=mvn-deps.ml'
-alias mdtocompile='mvn dependency:tree | grep ":compile" > "$(git symbolic-ref --short HEAD)-compile.txt"'
-alias mdtotest='mvn dependency:tree | grep ":test" > "$(git symbolic-ref --short HEAD)-test.txt"'
