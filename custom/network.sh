fdd3ffe4d0df7ed071352a451556f1f4b542150a
Samuel Huang <samhuang91@gmail.com>
2021-09-23 17:52:44 -0700

    Add cleardns to network.sh

--- custom/network.sh
+++ custom/network.sh
@@ -7,3 +7,6 @@ alias curlv='curl -fvso /dev/null'
 # My IP address
 alias curlip='curl bot.whatismyipaddress.com'
 alias digip='dig +short myip.opendns.com @resolver1.opendns.com'
+
+# Clear DNS cache on Mac OS
+alias cleardns='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
