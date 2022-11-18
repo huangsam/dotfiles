7d932cad33187b1bd858c0635e3f366606a540a7
Samuel Huang <samhuang91@gmail.com>
2022-11-18 10:24:44 -0800

    Change target URL of curlip command

--- custom/network.sh
+++ custom/network.sh
@@ -5,7 +5,7 @@ alias sshv='ssh -vvv -o LogLevel=DEBUG3'
 alias curlv='curl -fvso /dev/null'
 
 # My IP address
-alias curlip='curl bot.whatismyipaddress.com'
+alias curlip='curl icanhazip.com'
 alias digip='dig +short myip.opendns.com @resolver1.opendns.com'
 
 # Clear DNS cache on Mac OS
