a1da86aaa02e4d92214e8fdf2441d9a1f872c16f
Samuel Huang <samhuang91@gmail.com>
2022-11-18 09:52:34 -0800

    Remove unusable pipup command
    See https://github.com/pypa/pip/issues/9789 for rationale

--- custom/python.sh
+++ custom/python.sh
@@ -1,10 +1,3 @@
-# Update available pip dependencies
-function pipup() {
-    pip3 list --format=freeze --outdated \
-        | awk -F'==' '{print $1}' \
-        | xargs pip3 install -U
-}
-
 # Python helpers
 alias vwrap='source /usr/local/bin/virtualenvwrapper.sh'
 alias vmake='test -d venv || virtualenv venv'
