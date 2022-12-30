6bc785517e52be6d629a2df27e44323d2b096cdf
Samuel Huang <samhuang91@gmail.com>
2022-12-30 12:48:03 -0800

    Limit hstats digits to 2 places

--- custom/optional.sh
+++ custom/optional.sh
@@ -1,7 +1,7 @@
 # List top ten commands from history
 function hstats() {
     history \
-        | awk '{ CMD[$2]++; count++; } END { for (a in CMD) print CMD[a] " " CMD[a]/count*100 "% " a; }' \
+        | awk '{ CMD[$2]++; count++; } END { for (a in CMD) printf("%d %.2f%% %s\n", CMD[a], CMD[a]/count*100, a); }' \
         | grep -v './' \
         | column -c3 -s ' ' -t \
         | sort -nr \
