418acdb64c1708e476c4bdd98034e6982a55e6f3
Samuel Huang <samhuang91@gmail.com>
2022-12-30 12:56:49 -0800

    Remove vwrap from Python aliases

--- custom/python.sh
+++ custom/python.sh
@@ -1,5 +1,4 @@
 # Python helpers
-alias vwrap='source /usr/local/bin/virtualenvwrapper.sh'
 alias vmake='test -d venv || virtualenv venv'
 alias vunmake='test -d venv && rm -rf venv'
 alias vstart='source venv/bin/activate'
