48c40661283dc97d87c7fef34c0b34a14d37a4a3
Samuel Huang <samhuang91@gmail.com>
2024-04-04 03:07:01 -0700

    Adjust emojis on macos.zsh

--- macos.zsh
+++ macos.zsh
@@ -13,6 +13,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 compaudit | xargs chmod g-w,o-w
 
 # Indicate completion
-emoji_stars='\xE2\x9c\xa8'
-emoji_cake='\xF0\x9F\x8D\xB0'
+emoji_stars='\U00002728'
+emoji_cake='\U0001F370'
 echo -e "$0 complete! $emoji_stars $emoji_cake $emoji_stars"
