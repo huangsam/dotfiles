c5398d5a49120855b3bffe90be7c170ddf900da6
Samuel Huang <samhuang91@gmail.com>
2024-04-22 11:40:38 -0700

    Add alias for Python HTTP server

--- custom/python.zsh
+++ custom/python.zsh
@@ -3,3 +3,6 @@ alias vmake='test -d venv || virtualenv venv'
 alias vunmake='test -d venv && rm -rf venv'
 alias vstart='source venv/bin/activate'
 alias vstop='deactivate'
+
+# Python HTTP server
+alias pyhttp='python3 -m http.server --directory'
