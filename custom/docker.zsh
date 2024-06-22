a90cb7969fb06d68e447def0079e19716c803f92
Samuel Huang <samhuang91@gmail.com>
2024-06-21 21:24:22 -0700

    Simplify Docker alias collection

--- custom/docker.zsh
+++ custom/docker.zsh
@@ -1,24 +1,17 @@
 # Docker containers
-alias dps='docker ps'
-alias dpsa='docker ps -a'
-alias dlogs='docker logs'
-alias dexec='docker exec'
-alias drma='docker rm $(docker ps -qa)'
-alias dinip='docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"'
-alias dinim='docker inspect --format="{{range .NetworkSettings.Networks}}{{.MacAddress}}{{end}}"'
-alias dicfg='docker inspect --format="{{json .Config}}"'
+alias dcls='docker container ls'
+alias dcrm='docker container rm'
+alias dclg='docker container logs'
+alias dcex='docker container exec'
 
 # Docker images
-alias di='docker images'
-alias drmi='docker rmi'
-alias drmia='docker rmi $(docker images -qa)'
-alias drmid='docker rmi $(docker images -qf "dangling=true")'
-alias dpull='docker images --format "{{.Repository}}:{{.Tag }}" | xargs -L1 docker pull'
+alias dils='docker image ls'
+alias dirm='docker image rm'
 
 # Docker volumes
 alias dvls='docker volume ls'
-alias drmva='docker volume rm $(docker volume ls -q)'
-alias drmvd='docker volume rm $(docker volume ls -q -f "dangling=true")'
+alias dvrm='docker volume rm'
 
 # Docker networks
 alias dnls='docker network ls'
+alias dnrm='docker network rm'
