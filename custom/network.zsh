e7a1385d61fa0fed6294aa7a7fb4ded70b763312
Samuel Huang <samhuang91@gmail.com>
2024-06-21 22:13:38 -0700

    Add more network aliases

--- custom/network.zsh
+++ custom/network.zsh
@@ -7,3 +7,10 @@ alias curlv='curl -fvso /dev/null'
 # My IP address
 alias curlip='curl icanhazip.com'
 alias digip='dig +short myip.opendns.com @resolver1.opendns.com'
+
+# List of devices on the local network
+alias netlist='arp -a'
+
+# Network adapters
+alias ifdev='ifconfig en0'
+alias ifall='ifconfig -a'
