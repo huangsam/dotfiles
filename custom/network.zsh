6877de788bb60544fb11705df67b4fa764b93278
Samuel Huang <samhuang91@gmail.com>
2024-11-11 20:10:50 -0800

    Revise network alias module

--- custom/network.zsh
+++ custom/network.zsh
@@ -4,13 +4,11 @@ alias sshv='ssh -vvv -o LogLevel=DEBUG3'
 # HTTP/S connection
 alias curlv='curl -fvso /dev/null'
 
-# My IP address
-alias curlip='curl icanhazip.com'
-alias digip='dig +short myip.opendns.com @resolver1.opendns.com'
+# Public IP address
+alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
 
 # List of devices on the local network
 alias netlist='arp -a'
 
-# Network adapters
-alias ifdev='ifconfig en0'
-alias ifall='ifconfig -a'
+# Local Wi-Fi information
+alias localwifi='networksetup -getinfo Wi-Fi'
