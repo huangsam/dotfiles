3e0101ea6b02000a0510c5aedd4cd7b220de3c9b
Samuel Huang <samhuang91@gmail.com>
2024-11-11 20:12:19 -0800

    Add StackExchange link for myip

--- custom/network.zsh
+++ custom/network.zsh
@@ -5,6 +5,7 @@ alias sshv='ssh -vvv -o LogLevel=DEBUG3'
 alias curlv='curl -fvso /dev/null'
 
 # Public IP address
+# https://unix.stackexchange.com/a/335403
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
 
 # List of devices on the local network
