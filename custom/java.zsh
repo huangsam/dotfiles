fe6ab9aa699e3108f8cce9c9ea2a3888758c4b76
Samuel Huang <samhuang91@gmail.com>
2025-02-03 23:16:40 -0800

    Refactor more aliases across the board

--- custom/java.zsh
+++ custom/java.zsh
@@ -1,15 +1,15 @@
 # Run fresh Maven install
-alias mvci='mvn clean install'
+alias mvnci='mvn clean install'
 
 # Show Maven dependency tree
-alias mvdep='mvn dependency:tree'
+alias mvntree='mvn dependency:tree'
 
 # Skip Maven tests
-alias mvskip='mvn -DskipTests'
+alias mvnskip='mvn -DskipTests'
 
 # Setup Gradle assets for new project
-alias grnew='gradle init wrapper'
+alias gradnew='gradle init wrapper'
 
 # Show available Java JDKs
-alias jdkmacos='ls /Library/Java/JavaVirtualMachines'
+alias jdkapple='ls /Library/Java/JavaVirtualMachines'
 alias jdklinux='ls /usr/lib/jvm'
--- custom/macos.zsh
+++ custom/macos.zsh
@@ -3,13 +3,13 @@ alias macinfo='system_profiler SPHardwareDataType'
 
 # Mac OS updates
 alias macsoft='softwareupdate'
-alias softcheck='macsoft -l'
-alias softrun='sudo macsoft -i -a -R'
+alias macsoftlist='macsoft -l'
+alias macsoftrun='sudo macsoft -i -a -R'
 
 # Mac OS network
-alias cleardns='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
-alias timesync='sudo sntp -sS time.apple.com'
+alias macdnsflush='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
+alias mactimesync='sudo sntp -sS time.apple.com'
 
 # Mac OS files
-alias showall='defaults write com.apple.finder AppleShowAllFiles -bool TRUE && killall finder'
-alias hideall='defaults write com.apple.finder AppleShowAllFiles -bool FALSE && killall finder'
+alias macshow='defaults write com.apple.finder AppleShowAllFiles -bool TRUE && killall finder'
+alias machide='defaults write com.apple.finder AppleShowAllFiles -bool FALSE && killall finder'
