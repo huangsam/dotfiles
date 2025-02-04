905f90c130e9fa42bdbd9a45c3125da369dd279e
Samuel Huang <samhuang91@gmail.com>
2025-02-03 23:35:35 -0800

    Refactor gomo into a function

--- custom/golang.zsh
+++ custom/golang.zsh
@@ -1,5 +1,11 @@
 # Manage Go module dependencies
-alias gomoclean='go clean -modcache'
-alias gomodown='go mod download'
-alias gomotidy='go mod tidy'
-alias gomoup='go get -u ./...'
+gomo () {
+    cmd="${1:-unknown}"
+    case "$cmd" in
+        "clean") go clean -modcache ;;
+        "down") go mod download ;;
+        "tidy") go mod tidy ;;
+        "up") go get -u ./... ;;
+        *) echo "Invalid command: $cmd" ;;
+    esac
+}
