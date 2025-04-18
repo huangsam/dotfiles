ecd6594bde070afd308574fbadb8f04bdd4a089f
Samuel Huang <samhuang91@gmail.com>
2025-04-18 08:20:42 -0700

    Fix the script

--- combine.zsh
+++ combine.zsh
@@ -1,14 +1,21 @@
 #!/bin/zsh
 set -eu -o pipefail
 
-# Combine aliases and functions into one file
-# https://unix.stackexchange.com/a/541415/140057
-for fl in custom/*.zsh; do
-    cat "$fl"
-    echo
-done | perl -pe 'chomp if eof' > "$HOME/.zsh_aliases"
+# Options: default, override
+mode="${1:-default}"
+
+# Generate combined aliases if in default mode (file doesn't exist)
+# or override mode (file exists)
+if [ "${mode}" = "default" -a ! -f "$HOME/.zsh_aliases" ] || [ "${mode}" = "override" -a -f "$HOME/.zsh_aliases" ]; then
+    echo "Generate combined aliases"
+    # Combine aliases and functions into one file
+    # https://unix.stackexchange.com/a/541415/140057
+    cat custom/*.zsh | perl -pe 'chomp if eof' > "$HOME/.zsh_aliases"
+else
+    echo "Skip combined aliases"
+fi
 
 # Indicate completion
 emoji_stars='\U00002728'
 emoji_cake='\U0001F370'
-echo -e "$0 complete! $emoji_stars $emoji_cake $emoji_stars"
+echo -e "Script complete! $emoji_stars $emoji_cake $emoji_stars"
--- copy.zsh
+++ copy.zsh
@@ -1,16 +1,21 @@
 #!/bin/zsh
 set -eu
 
+# Options: default, override
+mode="${1:-default}"
+
 # Copy hidden files to home directory
 for fl in .*; do
-    if [[ -f "$fl" ]]; then
+    if [[ "$mode" == "override" && -f "$fl" ]]; then
+        cp -v -f "$fl" "$HOME/$fl"
+    elif [[ -f "$fl" ]]; then
         # Ignore non-zero exit status when a file is skipped
         # https://stackoverflow.com/a/38332629/2748860
-        cp -i "$fl" "$HOME/$fl" || :
+        cp -v -i "$fl" "$HOME/$fl" || :
     fi
 done
 
 # Indicate completion
 emoji_stars='\U00002728'
 emoji_cake='\U0001F370'
-echo -e "$0 complete! $emoji_stars $emoji_cake $emoji_stars"
+echo -e "Script complete! $emoji_stars $emoji_cake $emoji_stars"
