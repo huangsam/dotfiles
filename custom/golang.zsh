8c298872452b741d8078e2d557a097c1a3099ad3
Samuel Huang <samhuang91@gmail.com>
2025-11-10 18:59:50 -0800

    Shorten the gomo case statements

--- custom/golang.zsh
+++ custom/golang.zsh
@@ -2,11 +2,11 @@
 gomo () {
     local cmd="${1:-unknown}"
     case "$cmd" in
-        "clean") go clean -modcache ;;
-        "down") go mod download ;;
-        "tidy") go mod tidy ;;
-        "list") go list -m -f '{{if not .Indirect}}{{.Path}}{{end}}' all ;;
-        "outdated") go list -m -u -f '{{if and .Update (not .Indirect)}}{{.Path}} {{.Version}} -> {{.Update.Version}}{{end}}' all ;;
+        clean) go clean -modcache ;;
+        down) go mod download ;;
+        tidy) go mod tidy ;;
+        list) go list -m -f '{{if not .Indirect}}{{.Path}}{{end}}' all ;;
+        outdated) go list -m -u -f '{{if and .Update (not .Indirect)}}{{.Path}} {{.Version}} -> {{.Update.Version}}{{end}}' all ;;
         *) echo "Invalid command: $cmd" ;;
     esac
 }
