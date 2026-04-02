5b66a4a81c72994f00d5b1046df7340915543aea
Samuel Huang <samhuang91@gmail.com>
2026-04-01 23:11:52 -0700

    Remove outdated python aliases

--- custom/python.zsh
+++ custom/python.zsh
@@ -1,8 +1,2 @@
-# Python helpers
-alias vmake='test -d venv || virtualenv venv'
-alias vunmake='test -d venv && rm -rf venv'
-alias vstart='source venv/bin/activate'
-alias vstop='deactivate'
-
 # Python HTTP server
 alias pyhttp='python3 -m http.server --directory'
