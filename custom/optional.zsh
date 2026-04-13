4799408ea0bdf33232d8a85a4f85793514ceddbd
Samuel Huang <samhuang91@gmail.com>
2026-04-13 00:13:36 -0700

    Remove locstats from optional.zsh

--- custom/optional.zsh
+++ custom/optional.zsh
@@ -15,27 +15,6 @@ hstats () {
         }' | sort -nr | head -n10 | column -c3 -s ' ' -t | nl
 }
 
-# List line-of-code metrics for given directory and extension
-locstats () {
-    local target_dir="$1"
-    local extension="$2"
-    local pattern="*.${extension#.}"
-    if [[ ! -d "$target_dir" || -z "$extension" ]]; then
-        return 1
-    fi
-    {
-        local current=''
-        current=$(find "$target_dir" -maxdepth 1 -type f -name "$pattern" -exec cat {} + | wc -l)
-        echo ". $current"
-
-        find "$target_dir" -mindepth 1 -maxdepth 1 -type d -print0 | while IFS= read -r -d '' dir; do
-            local inner=''
-            inner=$(find "$dir" -type f -name "$pattern" -exec cat {} + | wc -l)
-            echo "$(basename "$dir") $inner"
-        done
-    } | column -t
-}
-
 # Reset Z shell configuration
 alias zset='source ~/.zshrc'
 
