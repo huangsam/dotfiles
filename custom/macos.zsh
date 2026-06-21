9afb011de558dc42fc4abd2bcbdca47df426b0a5
Samuel Huang <samhuang91@gmail.com>
2026-06-21 09:21:08 -0700

    Add macos alias helpers

--- custom/macos.zsh
+++ custom/macos.zsh
@@ -12,3 +12,9 @@ alias mactimesync='sudo sntp -sS time.apple.com'
 # Mac OS files
 alias macshow='defaults write com.apple.finder AppleShowAllFiles -bool TRUE && killall finder'
 alias machide='defaults write com.apple.finder AppleShowAllFiles -bool FALSE && killall finder'
+
+# Mac OS battery health
+alias macbattery='pmset -g batt'
+
+# Mac OS screenshot (save to desktop)
+alias macscreenshot='screencapture ~/Desktop/screenshot-$(date +%Y%m%d-%H%M%S).png'
