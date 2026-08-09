5b1ba2ecffc7f74e089f683adfae387b89df2a53
Samuel Huang <samhuang91@gmail.com>
2026-08-08 17:19:26 -0700

    Simplify completion of copy_configs

--- copy_configs.zsh
+++ copy_configs.zsh
@@ -16,6 +16,4 @@ for fl in .*; do
 done
 
 # Indicate completion
-emoji_stars='\U00002728'
-emoji_cake='\U0001F370'
-echo -e "$0 complete! $emoji_stars $emoji_cake $emoji_stars"
+echo -e "$0 complete! ✨ 🍰 ✨"
