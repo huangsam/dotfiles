20ebba3920ac7858c89b5f5fe9a969d051a01b63
Samuel Huang <samhuang91@gmail.com>
2026-08-13 04:48:32 -0700

    Remove trailing NL in aliases

--- combine_aliases.zsh
+++ combine_aliases.zsh
@@ -7,9 +7,11 @@ SCRIPT_DIR=${0:A:h}
 
 # Combine aliases and functions into one file
 {
+    first=1
     for fl in "$SCRIPT_DIR/custom/"*.zsh; do
+        (( first )) || print ""
         cat "$fl"
-        print ""
+        first=0
     done
 } > "$TARGET"
 
