5906f632abbb15aa632d63b5daf70567dbcfb8c8
Samuel Huang <samhuang91@gmail.com>
2026-08-13 04:44:07 -0700

    Simplify and harden combine_aliases

--- combine_aliases.zsh
+++ combine_aliases.zsh
@@ -1,14 +1,17 @@
 #!/bin/zsh
 set -eu -o pipefail
+setopt nullglob
 
 TARGET="$HOME/.zsh_aliases"
+SCRIPT_DIR=${0:A:h}
 
 # Combine aliases and functions into one file
-# https://unix.stackexchange.com/a/541415/140057
-for fl in custom/*.zsh; do
-    cat "$fl"
-    print ""
-done | perl -pe 'chomp if eof' > "$TARGET"
+{
+    for fl in "$SCRIPT_DIR/custom/"*.zsh; do
+        cat "$fl"
+        print ""
+    done
+} > "$TARGET"
 
 # Indicate completion
 print -r -- "Aliases combined into $TARGET"
