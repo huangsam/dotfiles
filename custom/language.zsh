675c5c8d7498c3db558fb2e54f0053bf9e7c897a
Samuel Huang <samhuang91@gmail.com>
2026-08-17 20:57:24 -0700

    Harden lang aliases and functions

--- custom/language.zsh
+++ custom/language.zsh
@@ -1,12 +1,13 @@
 # Manage Go module dependencies
 gomo() {
     local cmd="${1:-unknown}"
+    shift 2>/dev/null || true
     case "$cmd" in
-        clean) go clean -modcache ;;
-        down) go mod download ;;
-        tidy) go mod tidy ;;
-        list) go list -m -f '{{if not .Indirect}}{{.Path}}{{end}}' all ;;
-        outdated) go list -m -u -f '{{if and .Update (not .Indirect)}}{{.Path}} {{.Version}} -> {{.Update.Version}}{{end}}' all ;;
+        clean) go clean -modcache "$@" ;;
+        down) go mod download "$@" ;;
+        tidy) go mod tidy "$@" ;;
+        list) go list -m -f '{{if not .Indirect}}{{.Path}}{{end}}' all | grep -v '^$' ;;
+        outdated) go list -m -u -f '{{if and .Update (not .Indirect)}}{{.Path}} {{.Version}} -> {{.Update.Version}}{{end}}' all | grep -v '^$' ;;
         *)
             print -u2 -r -- "Error: Invalid command '$cmd'"
             print -u2 -r -- "Usage: gomo <clean|down|tidy|list|outdated>"
@@ -15,15 +16,13 @@ gomo() {
     esac
 }
 
-# Run fresh Maven install
-alias mvnci='mvn clean install'
-
-# Setup Gradle assets for new project
-alias gradnew='gradle init wrapper'
-
-# Show available Java JDKs
-alias jdkapple='ls /Library/Java/JavaVirtualMachines'
-alias jdklinux='ls /usr/lib/jvm'
-
 # Python HTTP server
-alias pyhttp='python3 -m http.server --directory'
+pyhttp() {
+    python3 -m http.server --directory "${1:-.}" "${2:-8000}"
+}
+
+# Python virtual environment shortcut
+alias pyvenv='python3 -m venv'
+
+# List installed macOS JDKs
+alias jdkls='/usr/libexec/java_home -V'
