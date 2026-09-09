c5e0c7ba47786a76a89c042f76e448493c917c65
Samuel Huang <samhuang91@gmail.com>
2026-09-09 08:28:21 -0700

    Remove extra space in macos.zsh

--- custom/macos.zsh
+++ custom/macos.zsh
@@ -5,6 +5,5 @@ alias macinfo='system_profiler SPHardwareDataType'
 alias macdnsflush='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
 alias mactimesync='sudo sntp -sS time.apple.com'
 
-
 # Mac OS caffeine (prevent sleep)
 alias macspike='caffeinate -u -t 43200'
